1c3bb40a7e7c69d208cfeb666355f35a25d19112
Deepak <deepakdatarta97@gmail.com>
2022-01-26 16:52:27 +0530

    Added Routes with file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Menu, Typography, Avatar } from "antd";
+import { Button, Menu, Typography, Avatar, Image } from "antd";
 import { Link } from "react-router-dom";
 import {
   HomeOutlined,
@@ -14,11 +14,27 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src={icon} size="large" />
+        <Avatar
+          src="https://lh3.googleusercontent.com/bGheJIL5TetmTzaYS395-aSMJyNQIUynA8ZBs6pV_Sn9BdkIsd8vSLEHYNN7etK6skSdpa6v9lQuzyphu4N04fuR0vHe1M8N4fHUfrXoug"
+          size="large"
+        />
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-        <div>hello</div>
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
 
         {/* <Button className='menu-control-container'>
 
